67ff07f7e4b07dd4d407fdfe573e6687c697fdb6
philpatt <phillip.s.patterson1@gmail.com>
2018-06-15 14:17:18 -0700

    accessing weather api

--- controllers/search.js
+++ controllers/search.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+
 router.get('/', function(req, res){
     console.log('SEARCH', req.query.location)
-    
+    var qs = {
+        q: req.query.location
+    }
+    request({
+        url: 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}',
+        q: qs
+    }, function(error, resposne, body){
+        console.log(body)
+    })
 })
 
 module.exports = router;
